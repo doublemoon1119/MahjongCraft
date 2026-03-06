2614da4d64b8aed22a4ca1ac09160f6752269529
doublemoon1119 <88666972+doublemoon1119@users.noreply.github.com>
2026-03-06 19:47:05 +0800

    build(application): expose coroutines as api dependency

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -17,8 +17,7 @@ dependencies {
     api(platform(libs.koin.bom))
     api(libs.koin.core)
 
-    // 僅用於編譯期，不打包進 Jar
-    compileOnly(libs.kotlin.coroutines)
+    api(libs.kotlin.coroutines)
 
     // 單元測試相關
     testImplementation(kotlin("test"))
